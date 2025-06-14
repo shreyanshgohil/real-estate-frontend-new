c1f2549178915aef54d14584461bf2ae402526c4
shreyansh <shreyansh@codalent.com>
2025-06-14 21:41:37 +0530

    feat: done

--- src/components/Pages/Home/PillarAgents/index.jsx
+++ src/components/Pages/Home/PillarAgents/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const PillarAgents = () => {
   return (
     <section className="team-area-1 space-bottom overflow-hidden">
       <div
         className="shape-mockup jump d-none d-lg-block"
+        data-top="6%"
+        data-right="4%"
         style={{ top: "6%", right: "4%" }}
       >
-        <img src="assets/img/shape/team-2-shape.png" alt="img" />
+        <img src="/images/shape/team-2-shape.png" alt="img" />
       </div>
       <div className="container">
         <div className="row justify-content-center">
@@ -33,100 +37,246 @@ const PillarAgents = () => {
         </div>
 
         <div className="slider-area mb-20">
-          <div
+          <Swiper
             className="swiper th-slider has-shadow"
             id="teamSlider1"
-            data-slider-options='{"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"1"},"768":{"slidesPerView":"2"},"992":{"slidesPerView":"2"},"1200":{"slidesPerView":"3"},"1400":{"slidesPerView":"4"}}, "autoHeight": "true"}'
+            autoHeight={true}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              576: { slidesPerView: 1 },
+              768: { slidesPerView: 2 },
+              992: { slidesPerView: 2 },
+              1200: { slidesPerView: 3 },
+              1400: { slidesPerView: 4 },
+            }}
           >
-            <div className="swiper-wrapper">
-              <div className="swiper-slide">
-                <div
-                  className="team-card style-2"
-                  data-bg-src="assets/img/shape/team-2-bg-shape.png"
-                >
-                  <div className="team-img">
-                    <img src="assets/img/team/team_2_1.png" alt="Team" />
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_1.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Vickie Wisozk</a>
+                    </h3>
+                    <span className="team-desig">Real Estate Manager</span>
                   </div>
-                  <div className="team-content">
-                    <div className="left-contet">
-                      <h3 className="box-title">
-                        <a href="team-details.html">Vickie Wisozk</a>
-                      </h3>
-                      <span className="team-desig">Real Estate Manager</span>
-                    </div>
-                    <div className="team-social">
-                      <div className="th-social">
-                        <a target="_blank" href="https://instagram.com/">
-                          <i className="fab fa-linkedin-in"></i>
-                        </a>
-                        <a target="_blank" href="https://linkedin.com/">
-                          <i className="fa-solid fa-envelope"></i>
-                        </a>
-                      </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
+            </SwiperSlide>
 
-              <div className="swiper-slide">
-                <div
-                  className="team-card style-2"
-                  data-bg-src="assets/img/shape/team-2-bg-shape.png"
-                >
-                  <div className="team-img">
-                    <img src="assets/img/team/team_2_2.png" alt="Team" />
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_2.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Renee Strosin</a>
+                    </h3>
+                    <span className="team-desig">Property Developer</span>
                   </div>
-                  <div className="team-content">
-                    <div className="left-contet">
-                      <h3 className="box-title">
-                        <a href="team-details.html">Renee Strosin</a>
-                      </h3>
-                      <span className="team-desig">Property Developer</span>
-                    </div>
-                    <div className="team-social">
-                      <div className="th-social">
-                        <a target="_blank" href="https://instagram.com/">
-                          <i className="fab fa-linkedin-in"></i>
-                        </a>
-                        <a target="_blank" href="https://linkedin.com/">
-                          <i className="fa-solid fa-envelope"></i>
-                        </a>
-                      </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
+            </SwiperSlide>
 
-              <div className="swiper-slide">
-                <div
-                  className="team-card style-2"
-                  data-bg-src="assets/img/shape/team-2-bg-shape.png"
-                >
-                  <div className="team-img">
-                    <img src="assets/img/team/team_2_3.png" alt="Team" />
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_3.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Zulia Era</a>
+                    </h3>
+                    <span className="team-desig">Listing Coordinator</span>
                   </div>
-                  <div className="team-content">
-                    <div className="left-contet">
-                      <h3 className="box-title">
-                        <a href="team-details.html">Zulia Era</a>
-                      </h3>
-                      <span className="team-desig">Listing Coordinator</span>
-                    </div>
-                    <div className="team-social">
-                      <div className="th-social">
-                        <a target="_blank" href="https://instagram.com/">
-                          <i className="fab fa-linkedin-in"></i>
-                        </a>
-                        <a target="_blank" href="https://linkedin.com/">
-                          <i className="fa-solid fa-envelope"></i>
-                        </a>
-                      </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_3.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Zulia Era</a>
+                    </h3>
+                    <span className="team-desig">Listing Coordinator</span>
+                  </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_3.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Zulia Era</a>
+                    </h3>
+                    <span className="team-desig">Listing Coordinator</span>
+                  </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_3.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Zulia Era</a>
+                    </h3>
+                    <span className="team-desig">Listing Coordinator</span>
+                  </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_3.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Zulia Era</a>
+                    </h3>
+                    <span className="team-desig">Listing Coordinator</span>
+                  </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="swiper-slide">
+              <div
+                className="team-card style-2"
+                data-bg-src="/images/shape/team-2-bg-shape.png"
+              >
+                <div className="team-img">
+                  <img src="/images/team/team_2_3.png" alt="Team" />
+                </div>
+                <div className="team-content">
+                  <div className="left-contet">
+                    <h3 className="box-title">
+                      <a href="team-details.html">Zulia Era</a>
+                    </h3>
+                    <span className="team-desig">Listing Coordinator</span>
+                  </div>
+                  <div className="team-social">
+                    <div className="th-social">
+                      <a target="_blank" href="https://instagram.com/">
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                      <a target="_blank" href="https://linkedin.com/">
+                        <i className="fa-solid fa-envelope"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
 
           <button
             data-slider-prev="#teamSlider1"
